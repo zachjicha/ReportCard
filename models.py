dd96ecd9ac25adf9cc4e5bd245276a374476ca5f
Zachary Jicha <zachjicha13@gmail.com>
2021-05-12 16:22:59 -0700

    add db

--- models.py
+++ models.py
@@ -20,5 +20,41 @@ def get_time():
 #
 ## always commit your models to avoid problems later
 
+db.define_table(
+    'schools',
+    Field('name', requires=IS_NOT_EMPTY()),
+)
+
+db.define_table(
+    'courses',
+    Field('name', requires=IS_NOT_EMPTY()),
+    Field('school', 'reference schools'),
+    Field('description', 'text'),
+)
+
+db.define_table(
+    'instructors',
+    Field('first_name', requires=IS_NOT_EMPTY()),
+    Field('last_name', requires=IS_NOT_EMPTY()),
+    Field('school', 'reference schools'),
+)
+
+db.define_table(
+    'course_reviews',
+    Field('course', 'reference courses'),
+    Field('review', 'text'),
+)
+
+db.define_table(
+    'instructor_reviews',
+    Field('course', 'reference instructor'),
+    Field('review', 'text'),
+)
+
+db.schools.id.readable            = db.schools.id.writable            = False
+db.courses.id.readable            = db.courses.id.writable            = False
+db.instructors.id.readable        = db.instructors.id.writable        = False
+db.course_reviews.id.readable     = db.course_reviews.id.writable     = False
+db.instructor_reviews.id.readable = db.instructor_reviews.id.writable = False
 
 db.commit()
